298f7268bb8147173a7055ca3f6fc20985738fd8
Manh-Hiep <hiepdm@vnu.edu.vn>
2024-08-19 06:50:57 +0700

    Minor Update: Writing mode for internal registers

--- verilog/dv/la_test1/la_test1.c
+++ verilog/dv/la_test1/la_test1.c
@@ -114,7 +114,9 @@ void main()
 	// Set Counter value to zero through LA probes [63:32]
 	reg_la1_data = 0xAB000000;
 
-	reg_la1_data = 0x00000000;
+	reg_la1_data = 0xA000000C;
+	reg_la1_data = 0xA001000B;
+
 	reg_la1_data = 0xAB400000;
 	// Configure LA probes from [63:32] as inputs to disable counter write
 	reg_la1_oenb = reg_la1_iena = 0x00000000;    
--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -36,189 +36,290 @@
  */
 
 module user_proj_example #(
-    parameter BITS = 16
+	parameter BITS = 16
 )(
 `ifdef USE_POWER_PINS
-    inout vccd1,	// User area 1 1.8V supply
-    inout vssd1,	// User area 1 digital ground
+	inout vccd1,	// User area 1 1.8V supply
+	inout vssd1,	// User area 1 digital ground
 `endif
 
-    // // Wishbone Slave ports (WB MI A)
-    input wb_clk_i,
-    input wb_rst_i,
+	// // Wishbone Slave ports (WB MI A)
+	input wb_clk_i,
+	input wb_rst_i,
 
-    // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output reg [127:0] la_data_out,
-    input  [127:0] la_oenb
+	// Logic Analyzer Signals
+	input  [127:0] la_data_in,
+	output reg [127:0] la_data_out,
+	input  [127:0] la_oenb
 );
-    wire clk;
-    wire rst;
-    reg master_enable, master_load;
-    wire slv_done;
-    wire [BITS-1:0] count;
-    wire [BITS-1:0] la_write;
-    
-    // LA
-    // assign la_data_out = (slv_done)? {{(128-BITS){1'b1}}, count}: {(128){1'b0}};
+	wire clk;
+	wire rst;
+	reg master_enable, master_load;
+	wire slv_done;
+	wire [BITS-1:0] count;
+	wire [BITS-1:0] la_write;
+	
+	reg [BITS-1:0] rega, regb;
+	// FSM Definition
+	reg [1:0]current_state, next_state;
+	parameter idle=2'b00, write_mode=2'b01, proc=2'b11, read_mode=2'b10;
+	reg master_start, read_done, master_read;
 
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:64-BITS];
+	// LA
+	// assign la_data_out = (slv_done)? {{(128-BITS){1'b1}}, count}: {(128){1'b0}};
 
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
+	// Assuming LA probes [63:32] are for controlling the count register  
+	assign la_write = ~la_oenb[63:64-BITS];
 
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .load(master_load),
-        .enable(master_enable),
-        .la_write(la_write),
-        .la_input(la_data_in[63:64-BITS]),
-        .count(count),
-        .done(slv_done)
-    );
+	// Assuming LA probes [65:64] are for controlling the count clk & reset  
+	assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
+	assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
 
-    always @(posedge clk or rst) begin
-        if (rst) begin
-            la_data_out <= {(128){1'b0}};
-        end else begin
-            if (slv_done) begin
-                la_data_out <= {{(128-BITS){1'b1}}, count};
-            // end else begin
-            //     la_data_out <= {(128){1'b0}};
-            end
-        end
-    end
+	counter #(
+		.BITS(BITS)
+	) counter(
+		.clk(clk),
+		.reset(rst),
+		.load(master_load),
+		.enable(master_enable),
+		.a(rega),
+		.b(regb),
+		.count(count),
+		.done(slv_done)
+	);
+	always @(posedge clk or rst) begin
+		if (rst) 
+			current_state <= idle;
+		else
+			current_state <= next_state;
+	end
 
-    always @(*) begin
-        if (la_data_in[63:48] == 16'hAB00) begin
-            master_enable <= 1'b0;
-            master_load <= 1'b1;
-        end else if ((la_data_in[63:48] == 16'hAB40)) begin
-            master_enable <= 1'b1;
-            master_load <= 1'b0;
-        end else begin
-            master_enable <= 1'b0;
-            master_load <= 1'b0;
-        end
-    end
+	always @(la_data_in or slv_done or read_done) begin
+		case (current_state)
+			idle: begin
+				if (la_data_in[63:48] == 16'hAB00) begin
+					next_state <= write_mode;
+				end else begin 
+					next_state <= idle;
+				end
+			end
+
+			write_mode: begin
+				if (la_data_in[63:48] == 16'hAB40) begin
+					next_state <= proc;
+				end else begin 
+					next_state <= write_mode;
+				end
+			end
+
+			proc: begin
+				if (slv_done) begin
+					next_state <= read_mode;
+				end else begin 
+					next_state <= proc;
+				end
+			end
+
+			read_mode: begin
+				if (read_done) begin
+					next_state <= idle;
+				end else begin
+					next_state <= read_mode;
+				end
+			end
+
+			default:
+			next_state <= idle;
+		endcase
+	end
+
+	always @(*) begin
+		case (current_state)
+			idle: begin
+				master_enable <= 1'b0;
+				master_load <= 1'b0;
+			end 
+
+			write_mode: begin
+				master_enable <= 1'b0;
+				master_load <= 1'b1;
+			end
+
+			proc: begin
+				master_enable <= 1'b1;
+				master_load <= 1'b0;
+			end
+
+			read_mode: begin
+				master_enable <= 1'b0;
+				master_load <= 1'b0;
+			end
+			default: begin
+				master_enable <= 1'b0;
+				master_load <= 1'b0;
+			end 
+		endcase 
+		// if (la_data_in[63:48] == 16'hAB00) begin
+		// 	master_enable <= 1'b0;
+		// 	master_load <= 1'b1;
+		// end else if ((la_data_in[63:48] == 16'hAB40)) begin
+		// 	master_enable <= 1'b1;
+		// 	master_load <= 1'b0;
+		// end else begin
+		// 	master_enable <= 1'b0;
+		// 	master_load <= 1'b0;
+		// end
+	end
+
+	always @(posedge clk or rst) begin
+		if (rst) begin
+			rega <= {{(BITS-1){1'b0}}};
+			regb <= {{(BITS-1){1'b0}}};
+			la_data_out <= {(128){1'b0}};
+			read_done <= 1'b0;
+		end else begin
+			case (current_state)
+				idle: begin
+					rega <= {{(BITS-1){1'b0}}};
+					regb <= {{(BITS-1){1'b0}}};
+					read_done <= 1'b0;
+				end 
+
+				write_mode: begin
+					if (la_data_in[63:48] == 16'hA000) begin
+						rega <= la_data_in[47:32];
+					end else if (la_data_in[63:48] == 16'hA001) begin
+						regb <= la_data_in[47:32];
+					end
+				end
+				
+				proc: begin
+					la_data_out <= {(128){1'b0}};
+				end
+
+				read_mode: begin
+					la_data_out <= {{(128-BITS){1'b1}}, count};
+					read_done <= 1'b1;
+				end
+				default: begin
+					rega <= {{(BITS-1){1'b0}}};
+					regb <= {{(BITS-1){1'b0}}};
+					la_data_out <= {(128){1'b0}};
+					read_done <= 1'b0;
+				end
+			endcase
+		end
+	end
 endmodule
 
 module counter #(
-    parameter BITS = 16
+	parameter BITS = 16
 )(
-    input clk,
-    input reset,
-    input load,
-    input enable,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output reg [BITS-1:0] count,
-    output reg done
+	input clk,
+	input reset,
+	input load,
+	input enable,
+	input [BITS-1:0] a,
+	input [BITS-1:0] b,
+	output reg [BITS-1:0] count,
+	output reg done
 );
-    reg [1:0]current_state, next_state;
-    reg [BITS-1:0] reg_count;
-    reg reg_enb_cnt, reg_load, reg_done;
-    parameter idle=2'b00, st_load=2'b01, proc=2'b11, st_done=2'b10;
+	reg [1:0]current_state, next_state;
+	reg [BITS-1:0] reg_count;
+	reg reg_enb_cnt, reg_load, reg_done;
+	parameter idle=2'b00, st_load=2'b01, proc=2'b11, st_done=2'b10;
 
-    always @(posedge clk or reset) begin
-        if (reset) 
-            current_state <= idle;
-        else
-            current_state <= next_state;
-        
-    end
+	always @(posedge clk or reset) begin
+		if (reset) 
+			current_state <= idle;
+		else
+			current_state <= next_state;
+	end
 
-    always @(load or enable or reg_done) begin
-        case (current_state)
-            idle: begin
-                if (load) begin
-                    next_state <= st_load;
-                end else begin 
-                    next_state <= idle;
-                end
-            end
+	always @(load or enable or reg_done) begin
+		case (current_state)
+			idle: begin
+				if (load) begin
+					next_state <= st_load;
+				end else begin 
+					next_state <= idle;
+				end
+			end
 
-            st_load: begin
-                if (enable) begin
-                    next_state <= proc;
-                end else begin  
-                    next_state <= st_load; 
-                end
-            end
+			st_load: begin
+				if (enable) begin
+					next_state <= proc;
+				end else begin  
+					next_state <= st_load; 
+				end
+			end
 
-            proc: begin
-                if (reg_done) begin
-                    next_state <= st_done;
-                end else begin 
-                    next_state <= proc;
-                end
-            end
+			proc: begin
+				if (reg_done) begin
+					next_state <= st_done;
+				end else begin 
+					next_state <= proc;
+				end
+			end
 
-            st_done: begin
-                next_state <= idle;
-            end
+			st_done: begin
+				next_state <= idle;
+			end
 
-            default:
-            next_state <= idle;
-        endcase
-    end
-    
-    always @(*) begin
-        case (current_state)
-            idle: begin
-                reg_load <= 1'b0;
-                reg_enb_cnt <= 1'b0;
-                done <= 1'b0;
-            end
+			default:
+			next_state <= idle;
+		endcase
+	end
+	
+	always @(*) begin
+		case (current_state)
+			idle: begin
+				reg_load <= 1'b0;
+				reg_enb_cnt <= 1'b0;
+				done <= 1'b0;
+			end
 
-            st_load: begin
-                reg_load <= 1'b1;
-                reg_enb_cnt <= 1'b0;
-                done <= 1'b0;
-            end
+			st_load: begin
+				reg_load <= 1'b1;
+				reg_enb_cnt <= 1'b0;
+				done <= 1'b0;
+			end
 
-            proc: begin
-                reg_load <= 1'b0;
-                reg_enb_cnt <= 1'b1;
-                done <= 1'b0;
-            end
+			proc: begin
+				reg_load <= 1'b0;
+				reg_enb_cnt <= 1'b1;
+				done <= 1'b0;
+			end
 
-            st_done: begin
-                reg_load <= 1'b0;
-                reg_enb_cnt <= 1'b0;
-                done <= 1'b1;
-            end
-        endcase
-    end
+			st_done: begin
+				reg_load <= 1'b0;
+				reg_enb_cnt <= 1'b0;
+				done <= 1'b1;
+			end
+		endcase
+	end
 
-    always @(posedge clk or reset) begin
-        if (reset) begin
-            reg_count <= 1'b0;
-            reg_done <= 1'b0;
-            count <= 1'b0;
-        end else begin
-            if (reg_enb_cnt) begin
-                if (reg_count < 1000) begin
-                    reg_count <= reg_count + 1'b1;
-                    reg_done <= 1'b0;
-                    // count <= 1'b0;
-                end else begin
-                    reg_count <= 1'b0;
-                    reg_done <= 1'b1;
-                    count <= reg_count;
-                end
-            end else if ((reg_load == 1'b1) && (enable == 1'b0)) begin
-                if (|la_write ) begin
-                    reg_count <= la_write & la_input;
-                end
-            end
-        end
-    end
+	always @(posedge clk or reset) begin
+		if (reset) begin
+			reg_count <= 1'b0;
+			reg_done <= 1'b0;
+			count <= 1'b0;
+		end else begin
+			if (reg_enb_cnt) begin
+				if (reg_count < 1000) begin
+					reg_count <= reg_count + 1'b1;
+					reg_done <= 1'b0;
+					// count <= 1'b0;
+				end else begin
+					reg_count <= 1'b0;
+					reg_done <= 1'b1;
+					count <= reg_count;
+				end
+			end else if ((reg_load == 1'b1) && (enable == 1'b0)) begin
+				reg_count <=  a;
+			end
+		end
+	end
 
 endmodule
 `default_nettype wire
